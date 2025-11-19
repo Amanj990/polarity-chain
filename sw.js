ddd6194c13db4c1bf3e75b5f09f0722656850b92
Amanj990 <91344777+Amanj990@users.noreply.github.com>
2025-11-19 16:45:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,33 @@
-self.addEventListener("install", event => {
+const CACHE_NAME = 'polarity-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './magnet.png',
+  './intro.mp4',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open("polarity-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./magnet.png",
-        "./intro.mp4"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  // Try cache first, then network
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(resp => resp || fetch(event.request).catch(()=>caches.match('./')))
   );
 });
